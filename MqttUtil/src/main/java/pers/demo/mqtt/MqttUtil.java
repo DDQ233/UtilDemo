6d6c0c5663814261f66b81d113435c8f13d827c1
DDQ <2841730354@qq.com>
2020-06-01 18:12:41 +0800

    新建 Project，新建 MqttUtil Module，新建 SerialPortUtil Module

---
+++ MqttUtil/src/main/java/pers/demo/mqtt/MqttUtil.java
@@ -0,0 +1,9 @@
+package pers.demo.mqtt;
+
+/**
+ * @Description TO-DO
+ * @Author DDQ
+ * @Date 2020/6/1 17:17
+ */
+public class MqttUtil {
+}
---
+++ SerialPortUtil/src/main/java/pers/demo/serialport/SerialPortUtil.java
@@ -0,0 +1,9 @@
+package pers.demo.serialport;
+
+/**
+ * @Description TO-DO
+ * @Author DDQ
+ * @Date 2020/6/1 18:09
+ */
+public class SerialPortUtil {
+}
