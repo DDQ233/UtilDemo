33566fc15aea633fd1f773132a73f00ac854ec68
DDQ233 <2841730354@qq.com>
2020-06-03 16:35:52 +0800

    添加了测试类进行测试

--- MqttUtil/src/test/java/pers/demo/mqtt/MqttUtilTest.java
+++ MqttUtil/src/test/java/pers/demo/mqtt/MqttUtilTest.java
@@ -1,5 +1,7 @@
 package pers.demo.mqtt;
 
+import org.eclipse.paho.client.mqttv3.MqttException;
+
 /**
  * @Description TO-DO
  * @Author DDQ
@@ -7,4 +9,20 @@ package pers.demo.mqtt;
  */
 public class MqttUtilTest {
 
+    public static void main(String[] args) throws MqttException {
+        String host = "mq.tongxinmao.com";
+        int port = 18831;
+        String clientId = "Client ID";
+        String username = "TEST";
+        String password = "TEST";
+        String topic = "/public";
+        int qos = 1;
+
+        MqttUtil mqttUtil = new MqttUtil();
+        mqttUtil.bindOptions(host, port, clientId, username, password);
+        mqttUtil.connect();
+        mqttUtil.setPushCallback(new PushCallback());
+        mqttUtil.subscribeTopic(topic,qos);
+
+    }
 }
